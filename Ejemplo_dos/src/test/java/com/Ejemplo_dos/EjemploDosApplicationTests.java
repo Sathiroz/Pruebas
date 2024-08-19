41366dd145fb01fbcdb2187c6efbf83a14be5743
Sathiroz <ramsespv25@gmail.com>
2024-08-18 19:02:56 -0600

    ﻿

--- Ejemplo_dos/src/test/java/com/Ejemplo_dos/EjemploDosApplicationTests.java
+++ Ejemplo_dos/src/test/java/com/Ejemplo_dos/EjemploDosApplicationTests.java
@@ -9,14 +9,17 @@ import static org.mockito.Mockito.when;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.Ejemplo_dos.entity.Ejemplo;
 import com.Ejemplo_dos.logic.EjemploLogic;
 import com.Ejemplo_dos.repository.EjemploRepository;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class EjemploDosApplicationTests {
 
